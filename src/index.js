870b498cb52c8878102621df0d839553133ed8ba
yachen <io260188@gmail.com>
2020-11-01 14:45:55 +0800

    feat: loading image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
-import moduleA from './module/moduleA.js'
-import '../assets/style.css'
+import moduleA from './module/moduleA.js';
+import '../assets/style.css';
+import ParrotImg from '../assets/parrot.jpeg';
 
-document.querySelector('.content-entry').innerText='這是 entry 檔案內容'
+function appendText(){
+  document.querySelector('.content-entry').innerText='這是 entry 檔案內容'
+}
+
+function appendImg(){
+  const parrotImgContainer = document.querySelector('.img-container')
+  const parrotImg = new Image();
+  parrotImg.src = ParrotImg
+
+  parrotImgContainer.appendChild(parrotImg)
+}
+
+appendText()
+appendImg()
 moduleA()
