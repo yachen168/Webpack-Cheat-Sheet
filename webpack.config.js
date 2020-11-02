5b867f31a57608a05cf94d018987b63816da0f6f
yachen <io260188@gmail.com>
2020-11-02 20:15:08 +0800

    feat: webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');   // 每次 build 前清理 /dist 文件夹
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new CleanWebpackPlugin()
-   ],
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    // publicPath: './dist',
+    contentBase: path.join(__dirname, 'dist'),
+    open: true
+  },
   module: {
     rules: [
       {
