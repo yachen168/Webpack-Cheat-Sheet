f4096ab807a2963a81c7e9927020fe806db91c3a
yachen <io260188@gmail.com>
2020-11-14 17:02:13 +0800

    feat: CssMinimizerWebpackPlugin

--- 03CSS壓縮/webpack.config.js
+++ 03CSS壓縮/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');   /* 每次 build 前清理 /dist 文件夹 */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); /* separate CSS from js */
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin'); /* CSS 壓縮 */
 
 module.exports = {
   mode: 'development',
@@ -38,16 +39,22 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif|jpeg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: false,
-          outputPath: 'images'
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash:8][ext]'
         }
       },
       {
         test: /\.html$/i,
         loader: 'html-loader',
       },
+    ],
+  },
+  optimization: {
+    /* If you want to run it also in development set the optimization.minimize option to true */
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
     ],
   },
 };
